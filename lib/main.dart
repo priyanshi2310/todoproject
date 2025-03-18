df39771955dfd486c3cb8c93275e826e4281f78f
priyanshi <sq.priyanshi23@gmail.com>
2025-03-18 17:05:18 +0530

    17-03

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todo_project/view/screens/home.dart';
+// import 'package:todo_project/view/screens/home.dart';
+import 'package:todo_project/view/todo/todo_screen.dart';
 // import 'package:todo_project/view/todo/todo_screen.dart';
 // import 'package:todo_project/view/todomodel/todo_model_screen.dart';
 
@@ -10,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  const Home(),
+      home: const ToDoScreen(),
     );
   }
 }
